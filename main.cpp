0abefa6768c5b5babb18f23ffea952eda920089c
mfim <matheusfim@hotmail.com>
2017-04-08 00:17:59 +0200

    [MINOR] added comments for greater readability
    Signed-off-by: mfim <matheusfim@hotmail.com>

--- main.cpp
+++ main.cpp
@@ -38,23 +38,32 @@ void visualize(uint16_t* PCM, uint16_t  size){
  //   iprintf("new\n");
     
     for(i = 0; i < size; i++){
-    //   	iprintf("%d:%d\n", (uint16_t) i, PCM[(uint16_t) i] );
+       	//iprintf("%d:%d\n", (uint16_t) i, PCM[(uint16_t) i] );
+     
         /* Real part, make offset by ADC / 2 */
-            input[(uint16_t)i*2] = (float32_t)((float32_t) PCM[i] - 32768);
-//            printf("%d:%f\n", i, (float32_t)input[(uint16_t)i] ); // prints key:val
-            input[(uint16_t)((i*2)+1)] = 0;
+        input[(uint16_t)i*2] = (float32_t)((float32_t) PCM[i] - 32768);
+        //printf("%d:%f\n", i, (float32_t)input[(uint16_t)i] ); // prints key:val
+        
+        /* imaginary part */    
+        input[(uint16_t)((i*2)+1)] = 0;
     }
 
+    /* Initialize the CFFT/CIFFT module, intFlag = 0, doBitReverse = 1 */
     arm_cfft_radix4_init_f32(&S, FFT_SIZE, 0, 1);
+
+    /* Process the data through the CFFT/CIFFT module */
     arm_cfft_radix4_f32(&S, input);
+    
+    /* Process the data through the Complex Magniture Module for calculating the magnitude at each bin */
     arm_cmplx_mag_f32(input, output, FFT_SIZE);
+    
+    /* Calculates maxValue and returns corresponding value */
     arm_max_f32(output, FFT_SIZE, &maxValue, &maxIndex);
 
-    printf("%f:%f\n", (float32_t) maxValue, (float32_t) output[(uint32_t)maxIndex]); // prints key:val
+    //printf("%f:%d\n", (float32_t) maxValue, (uint32_t)maxIndex); // prints key:val
 
 	  
     /*for(int i=0; i < size/2 ; i++ ){
-    	freq = i * 44100/ 256
     	printf("%d:%f\n", i, (float32_t)output[(uint16_t)i] ); // prints key:val
     }*/
 }
